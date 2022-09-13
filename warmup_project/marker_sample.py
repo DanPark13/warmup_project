6551a22c94fdbbc12e8970e38b663ba945c6cc3f
gabbyblake <gaby.blake2@gmail.com>
2022-09-12 21:08:26 -0400

    got sphere screenshot

--- warmup_project/marker_sample.py
+++ warmup_project/marker_sample.py
@@ -9,34 +9,34 @@ class SimpleVisualizationPublisher(Node):
         super().__init__('test_vis')
         self.vis_pub = self.create_publisher(Marker, 'visualization_marker', 10)
 
-        timer_period = 0.2 # seconds
+        timer_period = 0.1 # seconds
         self.timer = self.create_timer(timer_period, self.publish_marker)
     
     def publish_marker(self):
         marker = Marker()
-        marker.header.frame_id = "base_link";
+        marker.header.frame_id = "base_link"
         marker.header.stamp = self.get_clock().now().to_msg()
-        marker.ns = "my_namespace";
+        marker.ns = "my_namespace"
         marker.id = 0
 
-        marker.type = Marker.SPHERE;
-        marker.action = Marker.ADD;
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
         marker.pose.position.x = 1.0
-        marker.pose.position.y = 1.0
+        marker.pose.position.y = 2.0
         marker.pose.position.z = 1.0
-        marker.pose.orientation.x = 0.0;
-        marker.pose.orientation.y = 0.0;
-        marker.pose.orientation.z = 0.0;
-        marker.pose.orientation.w = 1.0;
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.pose.orientation.w = 1.0
         marker.scale.x = 1.0
-        marker.scale.y = 0.1
-        marker.scale.z = 0.1
-        marker.color.a = 1.0; # Don't forget to set the alpha!
-        marker.color.r = 0.0
+        marker.scale.y = 1.0
+        marker.scale.z = 1.0
+        marker.color.a = 1.0 # Don't forget to set the alpha!
+        marker.color.r = 1.0
         marker.color.g = 1.0
         marker.color.b = 0.0
 
-        self.vis_pub.publish( marker );
+        self.vis_pub.publish( marker )
 
 def main(args=None):
     rclpy.init(args=args)
